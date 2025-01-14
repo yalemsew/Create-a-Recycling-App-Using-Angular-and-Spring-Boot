6a27c0e74201af6a4b8287dd8816479449c638bd
Yalemsew Mekuriaw <yalemone@gmail.com>
2025-01-13 22:20:04 -0800

    model class defination

--- backend/src/main/java/com/example/backend/model/Product.java
+++ backend/src/main/java/com/example/backend/model/Product.java
@@ -1,4 +1,78 @@
 package com.example.backend.model;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+//import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 128)
+    private String name;
+
+    @Column(nullable = false, length = 4000)
+    private String description;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private double price;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date addedOn = new Date();
+
+    @Lob
+    @Column(nullable = true, length = Integer.MAX_VALUE)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private byte[] image;
+
+    public Product () {
+    }
+
+    public Product (String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Product (String name, String description, double price, byte[]image) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setAddedOn(Date addedOn) {
+        this.addedOn = addedOn;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
 }
---
+++ backend/src/main/java/com/example/backend/model/cart/CartItemPK.java
@@ -0,0 +1,4 @@
+package com.example.backend.model.cart;
+
+public class CartItemPK {
+}
